0294d7d5aa4ad3ae6b672d81a97d01769587a9a5
Chan Lee <chanlee514@gmail.com>
2016-11-01 14:37:47 -0700

    Update template for PIO v0.10.0-incubating

--- src/main/scala/Engine.scala
+++ src/main/scala/Engine.scala
@@ -1,7 +1,7 @@
 package org.template.vanilla
 
-import io.prediction.controller.IEngineFactory
-import io.prediction.controller.Engine
+import org.apache.predictionio.controller.IEngineFactory
+import org.apache.predictionio.controller.Engine
 
 case class Query(q: String) extends Serializable
 
